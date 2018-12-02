ebecba063e4a2c21ad780e5a37f9824cafd9852b
Shadowfacts <me@shadowfacts.net>
2018-12-02 10:29:12 -0500

    Day 2

---
+++ lib/day2/day2.ex
@@ -0,0 +1,76 @@
+defmodule Day2 do
+  def count_characters(id) do
+    id
+      |> String.graphemes()
+      |> Enum.reduce(%{}, fn c, acc ->
+        Map.update(acc, c, 1, &(&1 + 1))
+      end)
+  end
+
+  def checksum(list) do
+    characters = Enum.map(list, &count_characters/1)
+    two = Enum.count(characters, any_char_count(2))
+    three = Enum.count(characters, any_char_count(3))
+    two * three
+  end
+
+  defp any_char_count(n) do
+    fn map ->
+      map
+        |> Enum.to_list()
+        |> Enum.any?(fn {_c, count} -> count == n end)
+    end
+  end
+
+  def diff(a, b) do
+    a = String.graphemes(a)
+    b = String.graphemes(b)
+    Enum.zip(a, b)
+      |> Enum.reduce({0, ""}, fn {a, b}, {diff_count, common} ->
+        cond do
+          a == b ->
+            {diff_count, common <> a}
+          true ->
+            {diff_count + 1, common}
+        end
+      end)
+  end
+
+  def match(id, list) do
+    Enum.reduce_while(list, nil, fn other_id, _acc ->
+      case diff(id, other_id) do
+        {1, common} -> {:halt, common}
+        _ -> {:cont, nil}
+      end
+    end)
+  end
+
+  def correct_boxes(list) do
+    list
+      |> Enum.reduce_while(nil, fn id, _acc ->
+        case match(id, list) do
+          nil -> {:cont, nil}
+          common -> {:halt, common}
+        end
+      end)
+  end
+
+  def except(list, except) do
+    Enum.reject(list, fn el -> el == except end)
+  end
+
+  defp parse_input() do
+    File.read!("lib/day2/input.txt")
+      |> String.split("\n")
+  end
+
+  def part1() do
+    parse_input()
+      |> checksum()
+  end
+
+  def part2() do
+    parse_input()
+      |> correct_boxes()
+  end
+end
---
+++ test/day2_test.exs
@@ -0,0 +1,29 @@
+defmodule Day2Test do
+  use ExUnit.Case
+  doctest Day2
+
+  test "count characters" do
+    assert Day2.count_characters("abcdef") == %{"a" => 1, "b" => 1, "c" => 1, "d" => 1, "e" => 1, "f" => 1}
+    assert Day2.count_characters("bababc") == %{"a" => 2, "b" => 3, "c" => 1}
+    assert Day2.count_characters("abbcde") == %{"a" => 1, "b" => 2, "c" => 1, "d" => 1, "e" => 1}
+  end
+
+  test "checksum list" do
+    list = ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
+    assert Day2.checksum(list) == 12
+  end
+
+  test "diff" do
+    assert Day2.diff("fghij", "fguij") == {1, "fgij"}
+  end
+
+  test "match" do
+    list = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+    assert Day2.match("fghij", list) == "fgij"
+  end
+
+  test "correct boxes" do
+    list = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+    assert Day2.correct_boxes(list) == "fgij"
+  end
+end
